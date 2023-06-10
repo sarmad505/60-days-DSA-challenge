da23901d216836fab5b668c90017c01450999136
Sarmad Tufail <50269577+sarmad505@users.noreply.github.com>
2023-06-10 18:53:28 +0500

    Update Day 1 task.py

--- Day 1/Day 1 task.py
+++ Day 1/Day 1 task.py
@@ -7,26 +7,12 @@
 # You may assume that each input would have exactly one solution, and
 # you may not use the same element twice.
 # You can return the answer in any order.
-# 
-
-# In[7]:
 
 
-arrInt = [2,3,4]
-target = 7
-for i in arrInt:
-    #print(i,end=" ")
-    for j in arrInt:
-        print(j,end=" ")
 
-        if i + j ==target:
-            print("ok")
-        else:
-            print("not found")
  
 
 
-# In[52]:
 
 
 def arrSum(target,list):
